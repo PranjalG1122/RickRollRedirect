2a02da8cd943fb4d75bd98976d36c070fff5dd5c
Pranjal Gupta <pranjalg1122@gmail.com>
2023-12-01 18:55:23 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Rick Roll Redirect
+
+An extension that randomly rick rolls you whenever you go to YouTube.
---
+++ redirect.js
@@ -0,0 +1,3 @@
+if (Math.floor(Math.random() * 100) + 1 === 69) {
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
